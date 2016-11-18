178590193d6b7e82ac801631d37549192d09d97d
wellington.siqueira <wellingtonex@gmail.com>
2016-11-18 15:28:52 -0300

    criando projeto

---
+++ app.js
@@ -0,0 +1,12 @@
+let express = require('express');
+
+let app = express();
+
+app.get('/produtos', (req, res) => {
+    res.send('<html><body><h1>Listagem de produtos</h1></dody></html>');
+});
+
+app.listen(3000, () => {
+    console.log('servidor rodando');
+    
+})

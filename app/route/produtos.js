ff40c9d9f643c43d63b907ee8a982d9d6a01bd77
wellington.siqueira <wellingtonex@gmail.com>
2016-11-18 19:16:33 -0300

    acessando base de dados mysql

--- app/route/produtos.js
+++ app/route/produtos.js
@@ -1,5 +1,18 @@
+let mysql = require('mysql');
 module.exports = (app) => {
     app.get('/produtos', (req, res) => {
-        res.render("produtos/lista");
+        let connection = mysql.createConnection({
+            host:'localhost',
+            user:'root',
+            password:'root',
+            database: 'casadocodigo_nodejs'
+        });
+
+        connection.query('select * from livros', (error, result) => {
+            console.log(error);
+            res.send(result);
+        });
+        connection.end();
+        //res.render("produtos/lista");
     });
 }
